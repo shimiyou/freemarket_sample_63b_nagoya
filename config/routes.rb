e750d1403426f2cecc5f2246503f48047c09ac82
HirokiMinoura1991 <hirokiminoura.tech@gmail.com>
2019-11-26 18:55:01 +0900

    修正しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'items#index'
   resources :items, only: [:index, :new, :show, :edit]
-  resources :users, only: [:new, :create, :edit, :show]
+  resources :users, only: [:index,:new, :create, :edit, :show]
 end
