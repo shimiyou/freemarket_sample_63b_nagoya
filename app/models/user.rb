91cf6abe7b5e2a41511dc896b78e4f1a4957737c
shimiyou <0ri6ku1jo9apple@gmail.com>
2019-11-26 17:46:13 +0900

    userモデルを消してdeviseで生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  mount_uploader :image, ImageUploader
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
