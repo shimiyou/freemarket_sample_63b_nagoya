f792fa2077f47a1913a21e2c1cc725aa37834565
shimiyou <0ri6ku1jo9apple@gmail.com>
2019-12-02 13:34:04 +0900

    商品出品フォーム

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,8 +29,33 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :size_id, :state_id, :postage_side_id, :send_method_id, :prefecture_id, :send_date_id, :price, item_images_attributes: [:id, :image_url, :item_id]).to_h
+    params.require(:item).permit(:name, :description, :category_id, :size_id, :state_id, :postage_side_id, :send_method_id, :prefecture_id, :send_date_id, :price, item_images_attributes: [:image_url]).to_h
   end
+
+
+
+  # {"name"=>"モンサンミッシェル",
+  #   "description"=>"n",
+  #   "category_id"=>"3",
+  #   "size_id"=>"2",
+  #   "state_id"=>"5",
+  #   "postage_side_id"=>"2",
+  #   "send_method_id"=>"2",
+  #   "prefecture_id"=>"3",
+  #   "send_date_id"=>"3",
+  #   "price"=>"3000",
+  #   "item_images_attributes"=>
+  #    {"0"=>
+  #      {"image_url"=>
+  #        #<ActionDispatch::Http::UploadedFile:0x00007fd019f99200
+  #         @content_type="image/jpeg",
+  #         @headers=
+  #          "Content-Disposition: form-data; name=\"item[item_images_attributes][0][image_url]\"; filename=\"\xE3\x83\xA2\xE3\x83\xB3\xE3\x82\xB5\xE3\x83\xB3\xE3\x83\x9F\xE3\x83\x83\xE3\x82\xB7\xE3\x82\xA7\xE3\x83\xAB.jpg\"\r\nContent-Type: image/jpeg\r\n",
+  #         @original_filename="モンサンミッシェル.jpg",
+  #         @tempfile=#<File:/var/folders/sv/_dkg41752vqg350kr4g9fs940000gn/T/RackMultipart20191202-91578-1d1ny31.jpg>>}}}
+
+          
+
 
   def set_all
     @parents = Category.all.order("id ASC").limit(13)
