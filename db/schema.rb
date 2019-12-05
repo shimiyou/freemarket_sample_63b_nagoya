3a07f1e468c5f09eeea9deb397f42049f0bb0384
shimiyou <0ri6ku1jo9apple@gmail.com>
2019-12-05 16:51:39 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20191204110243) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "phone_number",                        null: false
     t.string   "provider"
     t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
