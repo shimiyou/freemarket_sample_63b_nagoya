4b85e0bab2f15f0ea75cd7ce423a16296b747fc7
YourName <you@example.com>
2019-12-09 10:52:07 +0900

    変更しました

--- app/assets/javascripts/item_show_image.js
+++ app/assets/javascripts/item_show_image.js
@@ -1,13 +1,12 @@
-$(function() {
+$(document).on('turbolinks:load',function() {
   $('.item__page__data__photo-box__photos__main').mouseover(function(){
-    var selectedSrc = $(this).attr('src')
-  $('.item__page__data__photo-box__photo__image').stop().fadeOut(100,
-    function(){
-    $('.item__page__data__photo-box__photo__image').attr('src', selectedSrc);
-    $('.item__page__data__photo-box__photo__image').stop().fadeIn(100);
-  }
-  );
-  $(this).css({"border":"1px solid #fff"});
+      var selectedSrc = $(this).attr('src')
+    $('.item__page__data__photo-box__photo__image').stop().fadeOut(230,
+        function(){
+        $('.item__page__data__photo-box__photo__image').attr('src', selectedSrc);
+        $('.item__page__data__photo-box__photo__image').stop().fadeIn(230);
+      }
+    );
   });
   $('.item__page__data__photo-box__photos__main').mouseout(function(){
     $(this).css({"border":""});
